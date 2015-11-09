34f27925df5a07e367464cdcf1dd59394234e306
Oskar Kärrman <05k4r@live.se>
2015-11-09 22:58:27 +0100

    Add basic storage.sync functionality

--- background.js
+++ background.js
@@ -2,6 +2,26 @@ var currentSong;
 var profile;
 var reposter;
 
+/*
+key: username
+value: {
+    totalLikes: int
+    totalDislikes: int
+    likes: [
+        {
+            profile: string
+            song: string
+        }
+    ]
+    dislikes: [
+        {
+            profile: string
+            song: string
+        }
+    ]
+}
+*/
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request.subject === 'getCurrentSong') {
 		var response = {
@@ -17,5 +37,16 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 		reposter = request.reposter;
 
 		sendResponse("Current song and profile updated");
+	} else if (request.subject === 'likeSong') {
+		chrome.storage.sync.set({'test': ['value1', 'value2']}, function() {
+			sendResponse("Liked!");
+		});
+		return true;
+	} else if (request.subject === 'dislikeSong') {
+		chrome.storage.sync.set({'test2': 'value'}, function() {
+			sendResponse('Disliked!');
+		});
+		return true;
 	}
+	return false;
 });
--- popup.js
+++ popup.js
@@ -9,3 +9,21 @@ chrome.runtime.sendMessage( {'subject': 'getCurrentSong'}, function(response) {
 
 	setChildTextNode('reposted-by', response.reposter);
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('like').addEventListener('click', likeSong);
+
+	document.getElementById('dislike').addEventListener('click', dislikeSong);
+});
+
+function likeSong() {
+	chrome.runtime.sendMessage( {'subject': 'likeSong'}, function(response) {
+		setChildTextNode('action', response);
+	});
+}
+
+function dislikeSong() {
+	chrome.runtime.sendMessage( {'subject': 'dislikeSong'}, function(response) {
+		setChildTextNode('action', response);
+	});
+}
