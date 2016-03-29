4f95b0c358368d929248f56ef0f0833d6924e5ab
Oskar Kärrman <05k4r@live.se>
2016-03-30 00:21:12 +0200

    Revert to browser action to be able to rate songs when playing in another tab

--- background.js
+++ background.js
@@ -284,24 +284,3 @@ function updateProfileData(profileName) {
 function arrayContains(array, value) {
     return array.indexOf(value) > -1;
 }
-
-// Following snippet borrowed from Google's Chrome Extension examples
-// When the extension is installed or upgraded ...
-chrome.runtime.onInstalled.addListener(function() {
-    // Replace all rules ...
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        // With a new rule ...
-        chrome.declarativeContent.onPageChanged.addRules([
-            {
-                // That fires when a page's URL contains 'soundcloud.com/stream' ...
-                conditions: [
-                    new chrome.declarativeContent.PageStateMatcher({
-                        pageUrl: { urlContains: 'soundcloud.com' },
-                    })
-                ],
-                // And shows the extension's page action.
-                actions: [ new chrome.declarativeContent.ShowPageAction() ]
-            }
-        ]);
-    });
-});
